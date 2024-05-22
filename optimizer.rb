3d9a66321a8c28f683441bba89085e8f4281e363
Javi Mata <mata@adview.mx>
2024-05-22 15:28:30 -0600

    feat: Update optimizer.rb to use 'optimize' as the binary name

--- optimizer.rb
+++ optimizer.rb
@@ -1,18 +1,18 @@
-class Optimiz < Formula
+class Optimize < Formula
   desc "Optimiza imágenes en la carpeta actual"
   homepage "https://github.com/javimata/homebrew-optimizer"
   url "https://github.com/javimata/homebrew-optimizer/releases/download/1.0/optimize_images-1.0.tar.gz"
-  sha256 "1ddbdbecdc808daf8a3f0723b719c9d5a3d6e9bd50c36bfaa6e8a6e31ff74679"
+  sha256 "893c8a847342c9338068dca0f93f1a27e4c2449916387725798ee8b476dc09af"
   license "MIT"
 
   depends_on "python@3.9"
 
   def install
-    bin.install "optimize_images.py" => "optimiz"
-    chmod 0755, "#{bin}/optimiz"
+    bin.install "optimize_images.py" => "optimize"
+    chmod 0755, "#{bin}/optimize"
   end
 
   test do
-    system "#{bin}/optimiz", "--version"
+    system "#{bin}/optimize", "--version"
   end
 end
