38348d5c9c801fa3d1e24c295ff457170ac44965
Javi Mata <mata@adview.mx>
2024-05-22 16:50:34 -0600

    feat: Update optimizer.rb to remove unused code and improve installation process

--- optimizer.rb
+++ optimizer.rb
@@ -1,6 +1,4 @@
 class Optimizer < Formula
-  include Language::Python::Virtualenv
-
   desc "Optimiza imágenes en la carpeta actual"
   homepage "https://github.com/javimata/homebrew-optimizer"
   url "https://github.com/javimata/homebrew-optimizer/releases/download/1.0/optimize_images-1.0.tar.gz"
@@ -8,13 +6,8 @@ class Optimizer < Formula
   license "MIT"
 
   depends_on "python@3.9"
-  depends_on "jpeg"
-  depends_on "zlib"
-  
+
   def install
-    venv = virtualenv_create(libexec, "python3")
-    venv.pip_install "pillow"
-    venv.pip_install_and_link buildpath
     bin.install "optimize_images.py" => "optimizer"
     chmod 0755, "#{bin}/optimizer"
   end
