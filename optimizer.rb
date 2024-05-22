79fda9952019da2d9f99e278a4a1f016eca8d18f
Javi Mata <mata@adview.mx>
2024-05-22 16:44:47 -0600

    feat: Add support for lazy loading images in optimize_images.py

--- optimizer.rb
+++ optimizer.rb
@@ -1,4 +1,6 @@
 class Optimizer < Formula
+  include Language::Python::Virtualenv
+
   desc "Optimiza imágenes en la carpeta actual"
   homepage "https://github.com/javimata/homebrew-optimizer"
   url "https://github.com/javimata/homebrew-optimizer/releases/download/1.0/optimize_images-1.0.tar.gz"
@@ -8,6 +10,9 @@ class Optimizer < Formula
   depends_on "python@3.9"
 
   def install
+    venv = virtualenv_create(libexec, "python3")
+    venv.pip_install "pillow"
+    venv.pip_install_and_link buildpath
     bin.install "optimize_images.py" => "optimizer"
     chmod 0755, "#{bin}/optimizer"
   end
