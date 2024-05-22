8e79fca8dc7a45dc7407dd8be43034a0a060372e
Javi Mata <mata@adview.mx>
2024-05-22 13:41:14 -0600

    update sha

--- optimizer.rb
+++ optimizer.rb
@@ -2,7 +2,7 @@ class Optimizer < Formula
   desc "Optimiza imágenes en la carpeta actual"
   homepage "https://github.com/javimata/homebrew-optimizer"
   url "https://github.com/javimata/homebrew-optimizer/releases/download/1.0/optimize_images-1.0.tar.gz"
-  sha256 "92af80e6abb3ca9f2622bf302e667d7f31275981dfcc759653c13a2d124ccb73"
+  sha256 "893c8a847342c9338068dca0f93f1a27e4c2449916387725798ee8b476dc09af"
   license "MIT"
 
   depends_on "python@3.9"
